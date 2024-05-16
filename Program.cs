d60e5e0e50751631837535443f1503d5ce6d3db2
Stefan Grushko <anion0278@gmail.com>
2024-05-16 22:04:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+
+string connectionString = "User=SYSDBA;Password=masterkey;Database=ExampleData.fdb;ServerType=1;";
+var con = new FbConnection(connectionString);
+con.Open();
+
+string sqlQuery = "SELECT * FROM \"Items\"";
+
+using (var command = con.CreateCommand())
+{
+    command.CommandText = sqlQuery;
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write(reader[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+        reader.Close();
+    }
+}
+
+con.Close();
+Console.ReadKey();
